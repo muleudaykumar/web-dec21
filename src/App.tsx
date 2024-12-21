3f11e2fc4475f6530e69bd8547c14737d7fba40e
muleudaykumar <muleudaykumar@gmail.com>
2024-12-21 10:51:04 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Products from './components/Products' 
+import EmpApi from './components/EmpApi'
+export default function App() {
+  return (
+    <div> 
+      
+      < EmpApi />
+      </div>
+  )
+}
+
